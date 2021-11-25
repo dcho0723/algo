30205d9ed2b34d86d0f1fe23c3fb6224debb2b38
dcho0723 <dcho0723@yahoo.com>
2021-11-25 16:29:21 -0500

    done

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/javascript/find_shortest_string_recursive.js
@@ -1,5 +1,29 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  //create a variable that volds length of string
+  //create variable that holds index of longest string
+  // create a for loop
+  // loop through each elemtn in aray, measuring the ;length of string
+  //create if statement, if length of string is less than our varialbe, add that index to our variable. 
+  //return the string
+  let strLeng = arr[0].length
+  let index = 0
+  // for (let i = 0; i < arr.length; i++) {
+  //   let curLength = arr[i].length
+  //   if(curLength < strLeng) {
+  //     index = i
+  //     strLeng = curLength
+  //   }
+  // }
+  // return arr[index]
+
+  for (let str of arr) {
+    let curLeng = str.length
+    if(curLeng < strLeng) {
+      index = arr.indexOf(str)
+      strLeng = curLeng
+    }
+  }
+  return arr[index]
 }
 
 if (require.main === module) {
