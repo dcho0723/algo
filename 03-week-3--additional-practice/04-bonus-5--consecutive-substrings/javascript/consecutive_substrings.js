7263aee9e7370b954ec7976ea1526d92594f2b86
dcho0723 <dcho0723@yahoo.com>
2021-12-12 14:32:52 -0500

    done

--- 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
+++ 03-week-3--additional-practice/04-bonus-5--consecutive-substrings/javascript/consecutive_substrings.js
@@ -1,18 +1,30 @@
 function consecutiveSubstrings(string) {
-  let returnArr = []
-  const newString = string.split("")
-  // console.log(newString)
+  // let returnArr = []
+  // const newString = string.split("")
+  // // console.log(newString)
 
-  newString.forEach((char, idx) => {
-    returnArr.push(char)
-    let fragment = char
+  // newString.forEach((char, idx) => {
+  //   returnArr.push(char)
+  //   let fragment = char
 
-    newString.slice(idx + 1).forEach((letter) => {
-      fragment = fragment + letter
-      returnArr.push(fragment)
-    })
-  })
-  return returnArr
+  //   newString.slice(idx + 1).forEach((letter) => {
+  //     fragment = fragment + letter
+  //     returnArr.push(fragment)
+  //   })
+  // })
+  // return returnArr
+
+  let newArr = []
+  for (let i = 0; i < string.length; i++) {
+    let currentIdx = i;
+    let newWord;
+    while (currentIdx < string.length) {
+      newWord = (newWord ? newWord : "") + string[currentIdx]
+      currentIdx++
+      newArr.push(newWord)
+    }
+  }
+  return newArr
 }
 
 if (require.main === module) {
